7ba274ee55fdbb97edb5a0ee17ad708c5d3452c4
Rene Hueber <rene.hub97@gmx.at>
2020-04-22 13:48:33 +0200

    split tray icon to a separate class

--- src/keylogger/Main.java
+++ src/keylogger/Main.java
@@ -15,92 +15,25 @@ import java.net.URL;
 public class Main extends Application {
     private Stage stage;
 
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.stage = primaryStage;
+        SystemTrayIcon systemTray = new SystemTrayIcon(stage);
         
         // instructs the javafx system not to exit implicitly when the last application window is shut.
         Platform.setImplicitExit(false);
         // sets up the tray icon (using awt code run on the swing thread).
-        javax.swing.SwingUtilities.invokeLater(this::addAppToTray);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                systemTray.addAppToTray();
+            }
+        });
+
         Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
         stage.setTitle("Key Strokes");
         stage.setScene(new Scene(root, 390, 395));
         stage.show();
-    }
-
-    protected static Image createIcon(String path, String desc){
-        URL ImageURL = Main.class.getResource(path);
-        return (new ImageIcon(ImageURL, desc)).getImage();
-    }
-    /**
-     * Sets up a system tray icon for the application.
-     */
-    private void addAppToTray() {
-        try {
-            // ensure awt toolkit is initialized.
-            java.awt.Toolkit.getDefaultToolkit();
-
-            // set's the tray Icon
-            java.awt.TrayIcon trayIcon = new java.awt.TrayIcon(createIcon("/images/Icon.png", "Tray Icon"));
-            trayIcon.setImageAutoSize(true);
-            trayIcon.setToolTip("Keylogger Stats");
-
-            final SystemTray tray = SystemTray.getSystemTray();
-
-            // app requires system tray support, just exit if there is no support.
-            if (!java.awt.SystemTray.isSupported()) {
-                System.out.println("No system tray support, application exiting.");
-                Platform.exit();
-            }
-
-            // if the user double-clicks on the tray icon, show the main app stage.
-            trayIcon.addActionListener(event -> Platform.runLater(this::showStage));
-
-            // if the user selects the default menu item (which includes the app name),
-            // show the main app stage.
-            java.awt.MenuItem openItem = new java.awt.MenuItem("Open");
-            openItem.addActionListener(event -> Platform.runLater(this::showStage));
-
-            // the convention for tray icons seems to be to set the default icon for opening
-            // the application stage in a bold font.
-            java.awt.Font defaultFont = java.awt.Font.decode(null);
-            java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
-            openItem.setFont(boldFont);
-
-            // to really exit the application, the user must go to the system tray icon
-            // and select the exit option, this will shutdown JavaFX and remove the
-            // tray icon (removing the tray icon will also shut down AWT).
-            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
-            exitItem.addActionListener(event -> {
-                Platform.exit();
-                tray.remove(trayIcon);
-            });
-
-            // setup the popup menu for the application.
-            final java.awt.PopupMenu popup = new java.awt.PopupMenu();
-            popup.add(openItem);
-            popup.addSeparator();
-            popup.add(exitItem);
-            trayIcon.setPopupMenu(popup);
-
-            // add the application tray icon to the system tray.
-            tray.add(trayIcon);
-        } catch (AWTException e) {
-            System.out.println("Unable to init system tray");
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Shows the application stage and ensures that it is brought ot the front of all stages.
-     */
-    private void showStage() {
-        if (stage != null) {
-            stage.show();
-            stage.toFront();
-        }
     }
 
     public static void main(String[] args) {
---
+++ src/keylogger/SystemTrayIcon.java
@@ -0,0 +1,94 @@
+package keylogger;
+
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class SystemTrayIcon {
+    private Stage stage;
+
+    public SystemTrayIcon(Stage stage){
+        this.stage = stage;
+    }
+
+    /**
+     * creates the Image for the System Tray
+     */
+    protected static Image createIcon(String path, String desc){
+        URL ImageURL = Main.class.getResource(path);
+        return (new ImageIcon(ImageURL, desc)).getImage();
+    }
+
+    /**
+     * Sets up a system tray icon for the application.
+     */
+    protected void addAppToTray() {
+        try {
+            // ensure awt toolkit is initialized.
+            java.awt.Toolkit.getDefaultToolkit();
+
+            // set's the tray Icon
+            java.awt.TrayIcon trayIcon = new java.awt.TrayIcon(createIcon("/images/Icon.png", "Tray Icon"));
+            trayIcon.setImageAutoSize(true);
+            trayIcon.setToolTip("Keylogger Stats");
+
+            final SystemTray tray = SystemTray.getSystemTray();
+
+            // app requires system tray support, just exit if there is no support.
+            if (!java.awt.SystemTray.isSupported()) {
+                System.out.println("No system tray support, application exiting.");
+                Platform.exit();
+            }
+
+            // if the user double-clicks on the tray icon, show the main app stage.
+            trayIcon.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // if the user selects the default menu item (which includes the app name),
+            // show the main app stage.
+            java.awt.MenuItem openItem = new java.awt.MenuItem("Open");
+            openItem.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // the convention for tray icons seems to be to set the default icon for opening
+            // the application stage in a bold font.
+            java.awt.Font defaultFont = java.awt.Font.decode(null);
+            java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
+            openItem.setFont(boldFont);
+
+            // to really exit the application, the user must go to the system tray icon
+            // and select the exit option, this will shutdown JavaFX and remove the
+            // tray icon (removing the tray icon will also shut down AWT).
+            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
+            exitItem.addActionListener(event -> {
+                Platform.exit();
+                tray.remove(trayIcon);
+            });
+
+            // setup the popup menu for the application.
+            final java.awt.PopupMenu popup = new java.awt.PopupMenu();
+            popup.add(openItem);
+            popup.addSeparator();
+            popup.add(exitItem);
+            trayIcon.setPopupMenu(popup);
+
+            // add the application tray icon to the system tray.
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("Unable to init system tray");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the application stage and ensures that it is brought ot the front of all stages.
+     */
+    private void showStage() {
+        if (stage != null) {
+            stage.show();
+            stage.toFront();
+        }
+    }
+}
+
