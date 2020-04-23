79015d0bbc96c169c492a34664a14bee9e642add
Rene Hueber <rene.hub97@gmx.at>
2020-04-23 19:09:44 +0200

    Layout changes, Add window to add keyboards

---
+++ src/gui/ControllerAddKeyboardWindow.java
@@ -0,0 +1,29 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import objects.Keyboards;
+
+import java.io.IOException;
+
+public class ControllerAddKeyboardWindow {
+    @FXML
+    private TextField input1, input2;
+
+
+    // passes the values pack to the select keyboard class and get's closed
+    public void passValues(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/selectKeyboardWindow.fxml"));
+        fxmlLoader.load();
+
+        ControllerSelectKeyboardWindow controller = fxmlLoader.getController();
+        controller.getKeyboardObject("Test", "Text");
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+}
--- src/gui/ControllerSelectKeyboardWindow.java
+++ src/gui/ControllerSelectKeyboardWindow.java
@@ -3,11 +3,17 @@ package gui;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import objects.Keyboards;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,7 +35,7 @@ public class ControllerSelectKeyboardWindow implements Initializable {
 
         keyboardsObservableList.addAll(
                 new Keyboards(ergoDox, "ErgoDox-Ez", 10000, 300.50f, "23.04.2020", "01.03.2020"),
-                new Keyboards(plank, "Planke", 6000, 100.50f, "18.04.2020", "01.01.2020"),
+                new Keyboards(plank, "Plank", 6000, 100.50f, "18.04.2020", "01.01.2020"),
                 new Keyboards(ergoDox, "ErgoDox-Ez Work", 35000, 1000.50f, "21.04.2020", "01.06.2019")
         );
     }
@@ -41,5 +47,27 @@ public class ControllerSelectKeyboardWindow implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         keyboardLv.setItems(keyboardsObservableList);
         keyboardLv.setCellFactory(KeyboardsListViewCell -> new KeyboardsListViewCell());
+    }
+
+    /**
+     * Open a new Window to add a new Keyboard
+     * @throws IOException If the is problem with the fxml loader
+     */
+    public void addKeyboardClicked() throws IOException {
+        // loads the fxml file
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/addKeyboardWindow.fxml"));
+        Parent rootAddKeyboard = (Parent) fxmlLoader.load();
+
+        // creates the stage and set's the property's
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Add Keyboard");
+        stage.setScene(new Scene(rootAddKeyboard));
+        stage.show();
+    }
+
+    public void getKeyboardObject(String test, String text){
+        System.out.println(test);
+        System.out.println(text);
     }
 }
