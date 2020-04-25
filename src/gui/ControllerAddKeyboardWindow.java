e6450169588b51ca48004e5ec4fd710f279394e1
Rene Hueber <rene.hub97@gmx.at>
2020-04-25 17:20:35 +0200

    Setup the date Picker

--- src/gui/ControllerAddKeyboardWindow.java
+++ src/gui/ControllerAddKeyboardWindow.java
@@ -13,8 +13,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ControllerAddKeyboardWindow {
     @FXML
@@ -26,8 +29,11 @@ public class ControllerAddKeyboardWindow {
     @FXML
     Label confirm;
 
+    /**
+     * Setup the Image View click Listeners, and the date picker.
+     */
     public void initialize() {
-        // change the layout if you click the image view
+        // change the layout if the split Layout image is clicked
         splitLayoutIv.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -35,32 +41,50 @@ public class ControllerAddKeyboardWindow {
             }
         });
 
-        // change the layout if you click the image view
+        // change the layout if you standard Layout image is clicked
         standardLayoutIv.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 changesScenes(mouseEvent, "../fxml/standardSelectedWindow.fxml");
             }
         });
-    }
 
-    // passes the values pack to the select keyboard class and get's closed
-    public void passValues(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/selectKeyboardWindow.fxml"));
-        fxmlLoader.load();
+        confirm.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                // keyboardConfirmed(mouseEvent);
+                checkInputs();
+            }
+        });
 
-        ControllerSelectKeyboardWindow controller = fxmlLoader.getController();
-        controller.getKeyboardObject("Test", "Text");
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        stage.close();
+        setupDatePicker();
     }
 
     /**
-     * Changes the Scenes in the current window
+     * Passes the entered Values to the SelectKeyboard Controller
+     * Closes the Window if finished
+     * @param event The Mouse Click event of the confirm label
+     */
+    public void keyboardConfirmed(MouseEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/selectKeyboardWindow.fxml"));
+            fxmlLoader.load();
+
+            ControllerSelectKeyboardWindow controller = fxmlLoader.getController();
+            controller.getKeyboardObject("Test", "Text");
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Changes the Scenes in the current window.
      * @param event The Mouse click event of the Image View
      * @param fxmlLayout Path to fxml file that should be loaded
      */
-    public void changesScenes(MouseEvent event, String fxmlLayout) {
+    private void changesScenes(MouseEvent event, String fxmlLayout) {
         // get's the current stage and changes the scene
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlLayout));
@@ -71,6 +95,48 @@ public class ControllerAddKeyboardWindow {
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Formats the Date like "dd.MM.yyyy" and set's the current Date.
+     */
+    private void setupDatePicker(){
+        // creates the formatter for the date of the date picker
+        StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
+            final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            @Override
+            public String toString(LocalDate localDate) {
+                if (localDate != null){
+                    return dateFormatter.format(localDate);
+                } else{
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String s) {
+                if (s != null && !s.isEmpty()){
+                    return LocalDate.parse(s, dateFormatter);
+                }
+                else{
+                    return null;
+                }
+            }
+        };
+        datePicker.setConverter(converter);
+        datePicker.setValue(LocalDate.now());
+        // user can only choose real dates
+        datePicker.setEditable(false);
+    }
+
+    // TODO finish this function
+    private void checkInputs(){
+        String keyboardName = name.getText();
+        if (keyboardName.isEmpty()){
+            System.out.println("False");
+        }
+        LocalDate useDate = datePicker.getValue();
+        System.out.println(String.format("%d.%d.%d", useDate.getDayOfMonth(), useDate.getMonthValue(), useDate.getYear()));
     }
 
 }
